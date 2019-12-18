49fbc32a9d0a80141d9420c0808fe53122f4b547
Malte Wirz <mail@maltewirz.com>
2019-12-18 10:41:26 +0100

    firebase working

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label> Username </label>
+                    <input class="form-control" type="text" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label> Mail </label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit()">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,24 @@
 
 <script>
     export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            };
+        },
+        methods: {
+            submit() {
+                this.$http.post('https://vue-http-8aa8a.firebaseio.com/data.json', this.user) //we have to add data.json for firebase
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(err => console.log(err))
+
+            }
+        }
     }
 </script>
 
